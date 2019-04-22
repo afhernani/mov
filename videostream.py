7c04f938e2ac05096454fab7bc1cad9e98bd6d44
afhernani <afhernani@gmail.com>
2019-04-22 18:22:16 +0100

    initial status by hernani

---
+++ videostream.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from ffpyplayer.player import MediaPlayer
+from PIL import Image
+import time
+import os
+
+__version__ = '1.0'
+
+class VideoStream:
+    def __init__(self, video_source=0):
+        ff_opts = {'paused' : True} # Audio options
+        # Open the video source
+        self.player = MediaPlayer(video_source, ff_opts=ff_opts)
+        # TODO: colocar pausa de tiempo para cargas mediaplayer y obtener los datos
+        # conseguir el frame rate para la sincronizacion self.dalay
+        while self.player.get_metadata()['src_vid_size'] == (0, 0):
+            time.sleep(0.01)
+        data  = self.player.get_metadata()
+        print('data -->', data)
+        self.f_rate = int(data['frame_rate'][0]/data['frame_rate'][1])
+        print('delay -> ', self.f_rate)
+        self.w, self.h = data['src_vid_size']
+        print('WxH -> ', self.w, self.h)
+        self.pts = self.player.get_pts() # Returns the elapsed play time. float
+        print('pts ->', self.pts)
+        self.pause = self.player.get_pause() # Returns whether the player is paused.
+        print('pause ->', self.pause)
+        self.volume = self.player.get_volume() # Returns the volume of the audio. loat: A value between 0.0 - 1.0
+        print('volume ->', self.volume)
+        self.player.toggle_pause() # Toggles -alterna- the player’s pause state
+        # self.player.set_pause(False) # auses or un-pauses the file. state: bool
+        cond = True
+        while cond:
+            self.l_frame, self.val = self.player.get_frame()
+            if self.val == 'eof':
+                print('can not open source: ', video_source)
+                break
+            elif self.l_frame is None:
+                time.sleep(0.01)
+            else:
+                self.imagen, self.t = self.l_frame
+                arr = self.imagen.to_memoryview()[0] # array image
+                self.imagen = Image.frombytes("RGB", (self.w, self.h), arr.memview)
+                # self.imagen.show()
+                cond = False
+        
+
+    def get_frame(self):
+        '''
+        Return valores:
+            val : 'eof' end sound
+            t : time location.
+            imagen : frame image
+        Return (val, t, imagen)
+        '''
+        # TODO: erase loop while do not anything really
+        while 1:
+            self.l_frame, self.val = self.player.get_frame()
+            if self.val == 'eof':
+                # condicion final fichero, salimos if and while
+                self.player.toggle_pause() # ponemos en pause
+                self.t = None
+                self.imagen = None
+                return self.val, self.t, self.imagen 
+            elif self.l_frame is None:
+                time.sleep(0.01)
+                return self.val, self.t, None
+            else:
+                self.imagen, self.t = self.l_frame
+                arr = self.imagen.to_memoryview()[0] # array image
+                self.imagen = Image.frombytes("RGB", (self.w, self.h), arr.memview)
+                # self.imagen.show()
+                return self.val, self.t, self.imagen
+        
+
+    def toggle_pause(self):
+        '''
+            Function: toggle_pause
+        '''
+        try: # Stopping audio
+            self.player.toggle_pause()
+            # self.player = None
+        except:
+            pass
+    
+    def snapshot(self):
+        '''
+        Nothing for now
+        '''
+        pass
+
+    # Release the video source when the object is destroyed
+    def __del__(self):
+        self.player.close_player()
+        print('__del__')
+
+
+if __name__ == '__main__':
+    video = VideoStream('_Work/tem.mp4')
---
+++ viewerplayer.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import tkinter as tk
+from tkinter import filedialog
+from PIL import ImageTk
+import time
+import os
+from videostream import VideoStream
+
+__version__ = '1.0'
+
+class ScreenPlayer:
+    def __init__(self, window, window_title, video_source=0):
+        ''' Initialice window app '''
+        self.window = window
+        self.window.title(window_title)
+        self.video_source = video_source
+        self.window['bg'] = 'Black'
+        self.window.resizable(width=False, height=False)
+        # open video source (by default this will try to open the computer webcam)
+        self.vid = VideoStream(self.video_source)
+        # Create a canvas that can fit the above video source size
+        self.canvas = tk.Canvas(window, width = self.vid.w, height = self.vid.h)
+        self.canvas.pack()
+        # Button that lets the user take a snapshot
+        self.btn_snapshot=tk.Button(window, text="Snapshot", command=self.snapshot)
+        self.btn_snapshot.pack(side='left')
+        # Button open
+        self.btn_open = tk.Button(window, text='...', command=self.open_file)
+        self.btn_open.pack(side='right')
+        # Button replay
+        self.btn_replay = tk.Button(window, text="Replay", command=self.replay)
+        self.btn_replay.pack(side='right')
+
+        # After it is called once, the update method will be automatically called every delay milliseconds
+        self.delay = self.vid.f_rate
+        self.update()
+
+        self.window.mainloop()
+
+    def open_file(self):
+        ''' Open file with menu ... '''
+        file = filedialog.askopenfile(initialdir='_Work', title='select file', 
+                                        filetypes={('mp4','*.mp4'), ('flv','*.flv')})
+        print(file)
+        if not file == None:
+            if os.path.exists(file.name):
+                self.video_source = file.name
+                self.replay()
+        
+
+    def replay(self):
+        ''' get replay '''
+        self.vid = VideoStream(self.video_source)
+        self.delay = self.vid.f_rate
+        # h = self.window.winfo_reqheight()
+        # w = self.window.winfo_reqwidth()
+        h_b = self.btn_open['height']
+        print('boton height ->', h_b)
+        h_f = int(self.vid.h + 35)
+        w_f = int(self.vid.w)
+        # wscale = w_f / w
+        # hscale = h_f / h
+        self.canvas.config(width=self.vid.w, height=self.vid.h)
+        #self.window.config(width=w_f, height=h_f)
+        # rescale all the objects tagged with the "all" tag
+        cad = f'{w_f}x{h_f}'
+        self.window.geometry(cad)
+
+    def snapshot(self):
+        '''
+        Get a frame from the video source
+        '''
+        pass
+ 
+    def update(self):
+        # Get a frame from the video source
+        frame = self.vid.get_frame()[2]
+
+        if frame is not None:
+            self.photo = ImageTk.PhotoImage(frame)
+            self.canvas.delete('all') # TODO: veamos que pasa con esto....
+            self.canvas.create_image(0, 0, image = self.photo, anchor = tk.NW)
+
+        self.window.after(self.delay, self.update)
+
+
+if __name__ == '__main__':
+    # Create a window and pass it to the Application object
+    ScreenPlayer(tk.Tk(), "Tkinter and ffpyplayer", video_source='_Work/tem.mp4')
