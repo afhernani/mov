3c7628cccf52a65e5767675a5c76fd66977d69df
afhernani <afhernani@gmail.com>
2019-04-25 16:00:29 +0100

    changed import tkinter as tk

--- photos.py
+++ photos.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from tkinter import *
+import tkinter as tk
 import os
 
 class Photos():
@@ -8,14 +8,14 @@ class Photos():
         print(self.dirname)
         self.dirname = os.path.dirname(self.dirname)
         print(self.dirname)
-        self._apply = PhotoImage(file=os.path.join(self.dirname, 'Images/apply_b.png'))
-        self._play = PhotoImage(file=os.path.join(self.dirname, 'Images/play_b.png'))
-        self._pause = PhotoImage(file=os.path.join(self.dirname, 'Images/pause_b.png'))
-        self._snapshot = PhotoImage(file=os.path.join(self.dirname, 'Images/snapshot_b.png'))
-        self._repeat = PhotoImage(file=os.path.join(self.dirname, 'Images/repeat_b.png'))
-        self._open = PhotoImage(file=os.path.join(self.dirname, 'Images/open_b.png'))
+        self._apply = tk.PhotoImage(file=os.path.join(self.dirname, 'Images/apply_b.png'))
+        self._play = tk.PhotoImage(file=os.path.join(self.dirname, 'Images/play_b.png'))
+        self._pause = tk.PhotoImage(file=os.path.join(self.dirname, 'Images/pause_b.png'))
+        self._snapshot = tk.PhotoImage(file=os.path.join(self.dirname, 'Images/snapshot_b.png'))
+        self._repeat = tk.PhotoImage(file=os.path.join(self.dirname, 'Images/repeat_b.png'))
+        self._open = tk.PhotoImage(file=os.path.join(self.dirname, 'Images/open_b.png'))
 
 
 if __name__ == '__main__':
-    root = Tk()
+    root = tk.Tk()
     p = Photos()
