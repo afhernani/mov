72dc83730f1a6915e0606898f5eabea70260b0f2
afhernani <afhernani@gmail.com>
2019-04-30 11:28:39 +0100

    no packages PIL here

--- photos.py
+++ photos.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import tkinter as tk
+from PIL import Image
 import os
 
 __author__ = 'Hernani Aleman Ferraz'
@@ -14,14 +15,13 @@ class Photos():
         self.dirname = os.path.dirname(self.dirname)
         print(self.dirname)
         self._apply = tk.PhotoImage(file=os.path.join(self.dirname, 'Images/apply_b.png'))
-        self._logo = tk.PhotoImage(file=os.path.join(self.dirname, 'Images/flash.png'))
         self._play = tk.PhotoImage(file=os.path.join(self.dirname, 'Images/play_b.png'))
         self._pause = tk.PhotoImage(file=os.path.join(self.dirname, 'Images/pause_b.png'))
         self._snapshot = tk.PhotoImage(file=os.path.join(self.dirname, 'Images/snapshot_b.png'))
         self._repeat = tk.PhotoImage(file=os.path.join(self.dirname, 'Images/repeat_b.png'))
         self._open = tk.PhotoImage(file=os.path.join(self.dirname, 'Images/open_b.png'))
         self._volume = tk.PhotoImage(file=os.path.join(self.dirname, 'Images/sound_b.png'))
-
+        self._logo = Image.open(os.path.join(self.dirname, 'Images/flash.png'))
 
 if __name__ == '__main__':
     root = tk.Tk()
